759f1eebbf239b8d7460ebed503a88c9815846dd
daminduLiyanage <damindu7@gmail.com>
2020-05-20 14:35:31 +0530

    objectified

--- newNode.py
+++ newNode.py
@@ -2,9 +2,10 @@ import requests
 from http.server import HTTPServer, BaseHTTPRequestHandler
 from io import BytesIO
 from threading import Thread
+import sys
 
-class SDTalk(BaseHTTPRequestHandler):
 
+class HTTPHandler(BaseHTTPRequestHandler):
     def do_GET(self):
         if self.path == '/':
             self.send_response(200)
@@ -16,30 +17,49 @@ class SDTalk(BaseHTTPRequestHandler):
             self.end_headers()
             self.wfile.write(b'reached path')
 
-    def do_POST(self):
-        content_length = int(self.headers['Content-Length'])
-        body = self.rfile.read(content_length)
-        self.send_response(200)
-        self.end_headers()
-        response = BytesIO()
-        response.write(b'This is POST request. ')
-        response.write(b'Received: ')
-        response.write(body)
-        self.wfile.write(response.getvalue())
 
 
-def server_thread():
-    httpd = HTTPServer(('localhost', 62224), SDTalk)
-    httpd.serve_forever()
+class SDServer:
+    def server_thread(self):
+        httpd = HTTPServer(('localhost', 62224), HTTPHandler)
+        httpd.serve_forever()
+
+    def server_start(self):
+        Thread(target=self.server_thread).start()
 
 
-def client_thread():
-    payload = {'user_name': 'admin', 'password': 'password'}
-    r = requests.get('http://localhost:62223/oops')
-    print(r.url)
-    print(r.text)
+class SDClient:
+
+    def client_thread(self):
+        payload = {'user_name': 'admin', 'password': 'password'}
+        r = requests.get('http://localhost:62223/oops')
+        print(r.url)
+        print(r.text)
+
+    def client_start(self):
+        Thread(target=self.client_thread).start()
 
 
-Thread(target=server_thread()).start()
-Thread(target=client_thread).start()
+# ==============================================================================
 
+
+def main(argv):
+    s = SDServer()
+    s.server_start()
+    c = SDClient()
+    c.client_start()
+
+
+if __name__ == "__main__":
+    main(sys.argv)
+
+    # def do_POST(self):
+    #     content_length = int(self.headers['Content-Length'])
+    #     body = self.rfile.read(content_length)
+    #     self.send_response(200)
+    #     self.end_headers()
+    #     response = BytesIO()
+    #     response.write(b'This is POST request. ')
+    #     response.write(b'Received: ')
+    #     response.write(body)
+    #     self.wfile.write(response.getvalue())
