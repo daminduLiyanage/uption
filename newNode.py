105674479cea90dd371a5fea69db1c603e47d047
daminduLiyanage <damindu7@gmail.com>
2020-05-20 15:20:41 +0530

    objectify complete with side class

--- newNode.py
+++ newNode.py
@@ -1,52 +1,113 @@
 import requests
 from http.server import HTTPServer, BaseHTTPRequestHandler
-from io import BytesIO
 from threading import Thread
 import sys
 
 
+# ==============================================================================
+# noinspection PyPep8Naming
 class HTTPHandler(BaseHTTPRequestHandler):
+    """
+        HTTP Request manager
+    """
+
+    # ------------------------------------------------------------------------
     def do_GET(self):
-        if self.path == '/':
+        # --------------------------------------------------------------------
+        if self.path == '/':    # return msg working with socket
             self.send_response(200)
             self.end_headers()
             self.wfile.write(b'Hello, world!')
 
-        if self.path == '/oops':
-            self.send_response(200)
-            self.end_headers()
-            self.wfile.write(b'reached path')
+        if self.path == '/requestID':   # requests for id
+            pass
 
 
+# ******************************************************************************
+class NDServer:
+    """
+        All server functions and server replies
+    """
 
-class SDServer:
-    def server_thread(self):
+    # ------------------------------------------------------------------------
+    def __init__(self, pid, port):
+        # --------------------------------------------------------------------
+        self.port = int(port)
+        self.pid = pid
+
+    # ------------------------------------------------------------------------
+    @staticmethod
+    def server_thread_load():
+        # --------------------------------------------------------------------
         httpd = HTTPServer(('localhost', 62224), HTTPHandler)
         httpd.serve_forever()
 
+    # ------------------------------------------------------------------------
     def server_start(self):
-        Thread(target=self.server_thread).start()
+        # --------------------------------------------------------------------
+        Thread(target=self.server_thread_load).start()
 
 
-class SDClient:
+# ==============================================================================
+class NDClient:
+    """
+        All requests made
+    """
 
-    def client_thread(self):
-        payload = {'user_name': 'admin', 'password': 'password'}
+    # ------------------------------------------------------------------------
+    def __init__(self):
+        # --------------------------------------------------------------------
+        self.pid_list = None
+        self.port_list = None
+        self.read_reg_flag = False
+
+    # ------------------------------------------------------------------------
+    def read_registry(self):
+        # --------------------------------------------------------------------
+        """
+        Usage pid_list, port_list = read_reg()
+        :return:
+        int[]:pid_list, int[]:port_list
+        """
+        if not self.read_reg_flag:
+            registry_filew = open("Registry.txt", "r")
+            line = registry_filew.read()
+            registry_filew.close()
+            broken = line.split(sep=' ')
+            broken.pop()
+            broken = [xw for xw in broken if xw != '\n']
+            left = broken[1::2]
+            right = broken[2::2]
+            self.pid_list = list(map(int, right))
+            self.port_list = list(map(int, left))
+            self.read_reg_flag = True
+            return self.pid_list, self.port_list
+        else:
+            return self.pid_list, self.port_list
+
+    # ------------------------------------------------------------------------
+    @staticmethod
+    def client_thread_load():
+        # --------------------------------------------------------------------
+        # payload = {'user_name': 'admin', 'password': 'password'}
         r = requests.get('http://localhost:62223/oops')
         print(r.url)
         print(r.text)
 
+    # ------------------------------------------------------------------------
     def client_start(self):
-        Thread(target=self.client_thread).start()
+        # --------------------------------------------------------------------
+        Thread(target=self.client_thread_load).start()
 
 
-# ==============================================================================
+# ******************************************************************************
 
-
+# ------------------------------------------------------------------------
 def main(argv):
-    s = SDServer()
+    # --------------------------------------------------------------------
+    s = NDServer(sys.argv[1], sys.argv[2])  # arg1 port, arg2 pid
     s.server_start()
-    c = SDClient()
+    c = NDClient()
     c.client_start()
 
 
--- newSide.py
+++ newSide.py
@@ -1,11 +1,16 @@
 from http.server import HTTPServer, BaseHTTPRequestHandler
 from io import BytesIO
 import socketserver
+import sys
 
 
-class SDTalk(BaseHTTPRequestHandler):
+# ==============================================================================
+# noinspection PyPep8Naming
+class HTTPHandler(BaseHTTPRequestHandler):
 
+    # ------------------------------------------------------------------------
     def do_GET(self):
+        # --------------------------------------------------------------------
         if self.path == '/':
             self.send_response(200)
             self.end_headers()
@@ -16,7 +21,12 @@ class SDTalk(BaseHTTPRequestHandler):
             self.end_headers()
             self.wfile.write(b'reached path')
 
+    # ------------------------------------------------------------------------
     def do_POST(self):
+        # --------------------------------------------------------------------
+        """
+            Not Used
+        """
         content_length = int(self.headers['Content-Length'])
         body = self.rfile.read(content_length)
         self.send_response(200)
@@ -28,5 +38,33 @@ class SDTalk(BaseHTTPRequestHandler):
         self.wfile.write(response.getvalue())
 
 
-httpd = HTTPServer(('localhost', 62223), SDTalk)
-httpd.serve_forever()
+# ******************************************************************************
+class SDServer:
+    """
+        All server functions
+    """
+    # ------------------------------------------------------------------------
+    def __init__(self):
+        # --------------------------------------------------------------------
+        pass
+
+    # ------------------------------------------------------------------------
+    @staticmethod
+    def start_server():
+        # --------------------------------------------------------------------
+        httpd = HTTPServer(('localhost', 62223), HTTPHandler)
+        httpd.serve_forever()
+
+
+# ==============================================================================
+# ------------------------------------------------------------------------
+def main(argv):
+    # --------------------------------------------------------------------
+    s = SDServer()
+    s.start_server()
+
+
+if __name__ == "__main__":
+    main(sys.argv)
+
+
