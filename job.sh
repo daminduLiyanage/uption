bf1a9dc46dc49ee86e871cec897c8476a93b282f
daminduLiyanage <damindu7@gmail.com>
2020-05-20 16:58:52 +0530

    tmux and auto server start ok not sure abt SCServer

--- job.sh
+++ job.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-python /Users/dumi/PycharmProjects/untitled3/MyNode.py $1 $2 | tee -a /Users/dumi/PycharmProjects/untitled3/side.out.txt
+python /Users/dumi/PycharmProjects/untitled3/newNode.py $1 $2 | tee -a /Users/dumi/PycharmProjects/untitled3/side.out.txt
